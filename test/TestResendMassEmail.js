07adac39a6b7a928e6f0635f93d333037f29adfb
Bryan Rockwood <bryan.rockwood@pollenware.com>
2011-06-15 14:00:00 -0500

    Checkin for yesterday's code review comments plus added logging to the mist-mailer server.  POLLEN-126

--- test/TestResendMassEmail.js
+++ test/TestResendMassEmail.js
@@ -1,11 +1,3 @@
-/**
- * Created by IntelliJ IDEA.
- * User: pollen
- * Date: 5/12/11
- * Time: 10:13 AM
- * To change this template use File | Settings | File Templates.
- */
-
 var RMJE = require("../lib/resendmassjangoemail.js");
 
 var fieldNames = ["FirstName","LastName","EmailAddress"];
@@ -13,12 +5,12 @@ var testContacts = JSON.parse("[ {\"FirstName\": \"Bryan\", \"LastName\": \"Rock
 var contactInfo = {"fieldNames" : fieldNames, "addresses" : testContacts};
 
 var test = new RMJE.ResendMassJangoEmail({
-    username : "username",
-    password : "password",
-    server : "api.jangomail.com",
-    jobId : 261496805,
-    contactInfo: contactInfo
-});
+            username : "username",
+            password : "password",
+            server : "api.jangomail.com",
+            jobId : 261496805,
+            contactInfo: contactInfo
+        });
 
 console.log(test.postArray);
 console.log("---------------------");
--- test/TestSendMassEmail.js
+++ test/TestSendMassEmail.js
@@ -1,26 +1,18 @@
-/**
- * Created by IntelliJ IDEA.
- * User: pollen
- * Date: 5/12/11
- * Time: 10:12 AM
- * To change this template use File | Settings | File Templates.
- */
-
 var SMJE = require("../lib/sendmassjangoEmail.js");
 var fieldNames = ["FirstName","LastName","EmailAddress"];
 var testContacts = JSON.parse("[ {\"FirstName\": \"Bryan\", \"LastName\": \"Rockwood\", \"EmailAddress\": \"bryan.rockwood@rockhouse.org\"}]");
 var contactInfo = {"fieldNames" : fieldNames, "addresses" : testContacts};
 
 var test = new SMJE.SendMassJangoEmail({
-    username : "username",
-    password : "password",
-    server : "api.jangomail.com",
-    subject : "Howdy Ho, %%FirstName%%!",
-    fromEmail : "node-jango@pollenware.com",
-    fromName : "New JangoMail Node Interface",
-    messagePlain : "Greetings %%FirstName%% %%LastName%%,\n\nThis is a test of the Node interface to JangoMail.  Your email address is %%EmailAddress%%.  Have a great day!\n\n\n -- BaseJangoRequest (BJR) OUT!",
-    contactInfo: contactInfo
-});
+            username : "username",
+            password : "password",
+            server : "api.jangomail.com",
+            subject : "Howdy Ho, %%FirstName%%!",
+            fromEmail : "node-jango@pollenware.com",
+            fromName : "New JangoMail Node Interface",
+            messagePlain : "Greetings %%FirstName%% %%LastName%%,\n\nThis is a test of the Node interface to JangoMail.  Your email address is %%EmailAddress%%.  Have a great day!\n\n\n -- BaseJangoRequest (BJR) OUT!",
+            contactInfo: contactInfo
+        });
 console.log(test.postArray);
 console.log("---------------------");
 test.sendToJango().then(function(res) {
