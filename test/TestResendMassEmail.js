dd2c1a1012e04b8adc9c433fc0e9c2a46553e193
Bryan Rockwood <bryan.rockwood@pollenware.com>
2011-06-16 08:50:55 -0500

    POLLEN-320 - Updated resend test script to use new class.

--- test/TestResendMassEmail.js
+++ test/TestResendMassEmail.js
@@ -1,4 +1,4 @@
-var RMJE = require("../lib/resendmassjangoemail.js")
+var EJU = require("../lib/jangojobutil.js"),
         vows = require('vows'),
         assert = require('assert');
 
@@ -6,34 +6,42 @@ var fieldNames = ["FirstName","LastName","EmailAddress"];
 var testContacts = JSON.parse("[ {\"FirstName\": \"Bryan\", \"LastName\": \"Rockwood\", \"EmailAddress\": \"bryan.rockwood@rockhouse.org\"}]");
 var contactInfo = {"fieldNames" : fieldNames, "addresses" : testContacts};
 
-var test = new RMJE.ResendMassJangoEmail({
-            username : "username",
-            password : "password",
-            server : "api.jangomail.com",
-            jobId : 261496805,
-            contactInfo: contactInfo
-        });
+var connectInfo = {
+    username : "username",
+    password: "password",
+    server: "api.jangomail.com",
+    jobId : 262567769,
+    contactInfo: contactInfo
+};
+
+var test = new EJU.JangoJobUtil(connectInfo);
 
 vows.describe("Resend mass JangoMail initialization ").addBatch({
             'when examining the username field' : {
-                topic: function(){ return test.postArray['Username']; },
+                topic: function() {
+                    return test.postArray['Username'];
+                },
                 "we get a value of 'username' back": function(topic) {
                     assert.equal(topic, "username")
                 }
             }, 'when examining the password field' : {
-                topic: function(){ return test.postArray['Password']; },
+                topic: function() {
+                    return test.postArray['Password'];
+                },
                 "we get a value of 'password' back": function(topic) {
                     assert.equal(topic, "password")
                 }
             }, 'when examining the jobId field' : {
-                topic: function(){ return test.postArray['JobID']; },
+                topic: function() {
+                    return test.postArray['JobID'];
+                },
                 "we get a value of 261496805 back": function(topic) {
                     assert.equal(topic, 261496805)
                 }
             }
         }).run({reporter : require("vows/reporters/spec")});
 
-/* test.sendToJango().then(function(res) {
+test.resendJob().then(function(res) {
     console.log(res);
-}); */
+});
 
