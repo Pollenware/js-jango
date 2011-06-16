7094742706a0388f245ee83481a0db430e280360
Bryan Rockwood <bryan.rockwood@pollenware.com>
2011-06-15 19:12:15 -0500

    POLLEN-129 - Updated TestResendMassEmail.js to use vows.

--- test/TestResendMassEmail.js
+++ test/TestResendMassEmail.js
@@ -1,4 +1,6 @@
-var RMJE = require("../lib/resendmassjangoemail.js");
+var RMJE = require("../lib/resendmassjangoemail.js")
+        vows = require('vows'),
+        assert = require('assert');
 
 var fieldNames = ["FirstName","LastName","EmailAddress"];
 var testContacts = JSON.parse("[ {\"FirstName\": \"Bryan\", \"LastName\": \"Rockwood\", \"EmailAddress\": \"bryan.rockwood@rockhouse.org\"}]");
@@ -12,9 +14,26 @@ var test = new RMJE.ResendMassJangoEmail({
             contactInfo: contactInfo
         });
 
-console.log(test.postArray);
-console.log("---------------------");
-test.sendToJango().then(function(res) {
+vows.describe("Resend mass JangoMail initialization ").addBatch({
+            'when examining the username field' : {
+                topic: function(){ return test.postArray['Username']; },
+                "we get a value of 'username' back": function(topic) {
+                    assert.equal(topic, "username")
+                }
+            }, 'when examining the password field' : {
+                topic: function(){ return test.postArray['Password']; },
+                "we get a value of 'password' back": function(topic) {
+                    assert.equal(topic, "password")
+                }
+            }, 'when examining the jobId field' : {
+                topic: function(){ return test.postArray['JobID']; },
+                "we get a value of 261496805 back": function(topic) {
+                    assert.equal(topic, 261496805)
+                }
+            }
+        }).run({reporter : require("vows/reporters/spec")});
+
+/* test.sendToJango().then(function(res) {
     console.log(res);
-});
+}); */
 
